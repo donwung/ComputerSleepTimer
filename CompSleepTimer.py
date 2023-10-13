4ad636d792e548cfeb4ecc3ebd1c1f1c6b87d781
donwung <113479299+donwung@users.noreply.github.com>
2023-10-12 20:52:44 -0700

    added dynamic field implemented grid for entry and button

--- CompSleepTimer.py
+++ CompSleepTimer.py
@@ -8,6 +8,7 @@ import threading
 
 # initialize Tk
 window = tk.Tk()
+window.geometry("200x250")
 
 
 class Number_Entry(tk.Frame):
@@ -17,9 +18,13 @@ class Number_Entry(tk.Frame):
 
         vcmd = (self.register(self.onValidate), '%d',
                 '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+
+        # Input and input debug
         self.entry = tk.Entry(self, validate="key", validatecommand=vcmd)
         self.text = tk.Text(self, height=10, width=40)
-        self.entry.pack(side="top", fill="x")
+        self.entry.grid(row="0", column="0")
+        self.submit = tk.Button(self, text="Button", command=self.get)
+        self.submit.grid(row="0", column="1")
         # self.text.pack(side="bottom", fill="both", expand=True)
 
     def onValidate(self, d, i, P, s, S, v, V, W):
@@ -54,55 +59,64 @@ class Number_Entry(tk.Frame):
         else:
             print(self.STR)
 
-
 def radio():
+    pass
     # this creates a new widget, placed *after* all other created widgets (it appears at the bottom always)
     # create and package a label
-    option_label = tk.Label(window, text="Testing")
-    option_label.pack()
+    # option_label = tk.Label(window, text="Testing")
+    # option_label.pack()
 
     
-    selection = "You selected to "
-    if (radio_option.get()):
-        _selection = selection + " sleep from a timer."
-    else:
-        _selection = selection + " sleep at a specified time."
-    option_label.config(text=_selection)
+    # selection = "You selected to "
+    # if (radio_option.get()):
+    #     _selection = selection + " sleep at a specified time."
+    # else:
+    #     _selection = selection + " sleep from a timer."
+    # option_label.config(text=_selection)
+
+
 
     # remove label after 2000 ms
-    window.after(2000, option_label.destroy)
+    # window.after(2000, option_label.destroy)
+
+def clock():
+    currenttime.config(text="Current time: " +
+                       str(datetime.datetime.now().hour) + ":" +
+                       str(datetime.datetime.now().minute) + ":" +
+                       str(datetime.datetime.now().second))
+    currenttime.after(1000, clock)
 
 
 
 
-def enable_sleep(sleep):
-    if (int(sleep) > 300000):
-        messagebox.showinfo(
-            "Sleep Enabled", "Your computer is set to sleep in " + sleep + "ms")
-    else:
-        messagebox.showerror(
-            "Sleep Not Enabled", "Please enter a reasonable time to sleep your computer (more than 300000ms).")
+# def enable_sleep(sleep):
+#     if (int(sleep) > 300000):
+#         messagebox.showinfo(
+#             "Sleep Enabled", "Your computer is set to sleep in " + sleep + "ms")
+#     else:
+#         messagebox.showerror(
+#             "Sleep Not Enabled", "Please enter a reasonable time to sleep your computer (more than 300000ms).")
 
 
-def invalid_input():
-    messagebox.showerror("Invalid Time", "Enter a valid time in ms.")
+# def invalid_input():
+#     messagebox.showerror("Invalid Time", "Enter a valid time in ms.")
 
 
 # parse input
-def printtext():
-    print("Disabling current sleep timer...")
-    sleep = sleeptime.get()
-    # print("parsing input: " + sleep)
-    if (sleep.isnumeric()):
-        print("sleeping in " + sleep + "ms")
-        enable_sleep(sleep)
-    else:
-        print("not a valid ms input!")
-        invalid_input()
+# def printtext():
+#     print("Disabling current sleep timer...")
+#     sleep = sleeptime.get()
+#     # print("parsing input: " + sleep)
+#     if (sleep.isnumeric()):
+#         print("sleeping in " + sleep + "ms")
+#         # enable_sleep(sleep)
+#     else:
+#         print("not a valid ms input!")
+#         # invalid_input()
 
 
-def cmdtest():
-    print("testing cmd")
+# def cmdtest():
+#     print("testing cmd")
     # os.system('cmd /c "echo Hello World!"')
     # don't use shutdown -- it doesn't allow sleep
     # os.system('cmd /c "psshutdown -d -t 20"')
@@ -118,21 +132,39 @@ def cmdtest():
 ####################
 ####################
 # label
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack()
+# greeting = tk.Label(text="Hello, Tkinter")
+# greeting.pack()
 
-
+# Shows current time
+# TODO: format this to be more readable + add font
 currenttime = tk.Label(text=datetime.datetime.now())
 currenttime.pack()
 
-sleeptimelabel = tk.Label(
-    text="In how many ms would you like to sleep your computer?")
-sleeptimelabel.pack()
-sleeptime = Number_Entry(window)
-sleeptime.pack()
 
+# sleeptimelabel = tk.Label(text="In how many ms would you like to sleep your computer?")
+# sleeptimelabel.pack()
 
 radio_option = tk.IntVar()
+
+entry_for_sleep_in_time = Number_Entry(window)
+# entry_for_sleep_at_time = Number_Entry(window)
+entry_for_sleep_at_time = tk.Label(text="testing text - not a field")
+
+sleep_input_label = tk.Label(text="SLEEP IN TIME")
+
+def update_input_field(*args):
+    if(radio_option.get()):
+        entry_for_sleep_at_time.pack()
+        entry_for_sleep_in_time.pack_forget()
+        sleep_input_label.config(text="SLEEP AT TIME")
+    else:
+        entry_for_sleep_at_time.pack_forget()
+        entry_for_sleep_in_time.pack()
+        sleep_input_label.config(text="SLEEP IN TIME")
+
+radio_option.trace('w', update_input_field)
+
+
 sleep_in_time = tk.Radiobutton(
     window, text="Sleep starting from a timer.",
     variable=radio_option,
@@ -146,34 +178,19 @@ sleep_at_time = tk.Radiobutton(
     command=radio)
 sleep_at_time.pack()
 
+sleep_input_label.pack()
 
-# button
-# testbtn = tk.Button(
-#     text="Set To Sleep",
-#     width=20,
-#     height=5,
-#     command=printtext)
-# testbtn.pack()
-
-testbtn2 = tk.Button(
-    text="Prints number in Entry field",
-    command=sleeptime.get
-)
-testbtn2.pack()
+# testbtn2 = tk.Button(
+#     text="Prints number in Entry field",
+#     command=entry_for_sleep_in_time.get
+# )
+# testbtn2.pack()
 
 
-def clock():
-    currenttime.config(text="Current time: " +
-                       str(datetime.datetime.now().hour) + ":" +
-                       str(datetime.datetime.now().minute) + ":" +
-                       str(datetime.datetime.now().second))
-    currenttime.after(1000, clock)
-
-
-# option_label = tk.Label(window)
-# option_label.pack()
 
 clock()
+update_input_field()
+
 
 # TODO: create alert on OK button
 # TODO: change UI elements based on which radio button was pressed

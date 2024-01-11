01e86c80f9577e753685d9589e57c2354df43b5f
donwung <113479299+donwung@users.noreply.github.com>
2024-01-10 16:44:44 -0800

    created sleep manager

--- App.py
+++ App.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from Clock import Clock
+from Input_Field import Input_Field
+from Sleep_Manager import Sleep_Manager
 
 
 window = tk.Tk()
+
 
 # TODO: create clock DONE!
 # set a time to go to sleep IN
@@ -17,9 +20,20 @@ window = tk.Tk()
 
 
 clock = Clock(window)
+hms_input_field = Input_Field(window)
+sleeper = Sleep_Manager(hms_input_field)
+
+
+receive_time_input = tk.Button(text="receive_time_input", command=sleeper.receive_time)
+receive_time_input.pack()
+
+test_go_to_sleep = tk.Button(text="test_go_to_sleep", command=sleeper.go_to_sleep)
+test_go_to_sleep.pack()
+
+
 
 # this is test text - irrelevant
-greeting = tk.Label(text="Hello World!")
-greeting.pack()
+# greeting = tk.Label(text="Hello World!")
+# greeting.pack()
 
 window.mainloop()

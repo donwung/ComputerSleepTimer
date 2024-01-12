27df8f2ddc5ec785d8160dbffad2d03e574f6103
donwung <113479299+donwung@users.noreply.github.com>
2024-01-12 15:54:06 -0800

    added sleep command

--- Sleep_Manager.py
+++ Sleep_Manager.py
@@ -1,16 +1,22 @@
+import os
+import ctypes
+
+
 class Sleep_Manager:
     def __init__(self, input_field):
         print("Sleep_Manager instantiated")
         self.input_field = input_field
         self.time_dict = {"hours": 0, "minutes": 0, "seconds": 0}
+        self.countdown_str = "countdown string"
 
-    # TODO: make another class to receive time to go to sleep AT
-
-    def receive_time(self):
+    def set_time_input(self):
         print("receiving a time")
         new_time = self.input_field.get_input_time()
         self.time_dict = new_time
         print(self.time_dict)
+
+    def get_time_input(self):
+        return self.time_dict
 
     def convert_to_ms(self, time_dict):
         hours_to_ms = int(time_dict["hours"]) * 3600000
@@ -21,17 +27,23 @@ class Sleep_Manager:
         # print(f"{str(time_dict['seconds'])} hour -> {seconds_to_ms} milliseconds")
 
         time_to_sleep_in_ms = hours_to_ms + minutes_to_ms + seconds_to_ms
-        print(time_to_sleep_in_ms)
+        # print(time_to_sleep_in_ms)
         return time_to_sleep_in_ms
 
-    def set_sleep_time(self):
-        print("setting sleep time")
+    def start_countdown(self):
+        print("setting countdown")
+        print(f"sleeping in: {str(self.convert_to_ms(self.time_dict))} ms")
+        self.countdown_str = {str(self.convert_to_ms(self.time_dict))}
+        # in X time, I will sleep
+        print(self.time_dict)
+        return self.time_dict
 
     def go_to_sleep(self):
-        print("COMPUTER IS BEING PUT TO SLEEP")
-        print(f"sleeping in: {str(self.convert_to_ms(self.time_dict))} ms")
+        print("COMPUTER IS BEING PUT TO SLEEP - GOODNIGHT")
+        os.system('cmd /c "cmd going to sleep"')
+        ctypes.windll.powrprof.SetSuspendState(0, 1, 0)
 
 
-# TODO: 
-# TODO: 
+# TODO:
+# TODO:
 # TODO: create an inherited class to set sleep using sleep AT

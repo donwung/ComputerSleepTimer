7726bfb2d9c32381d3dd7675494f5e9c639a6820
donwung <113479299+donwung@users.noreply.github.com>
2024-02-05 18:50:27 -0800

    removed comments, refactored main

--- App.py
+++ App.py
@@ -3,50 +3,21 @@ from Clock import Clock
 from Input_Field import Input_Field
 from Sleep_Manager import Sleep_Manager
 from Countdown import Countdown
+from tkinter.font import Font
 
 
 window = tk.Tk()
 
-
-# TODO: create clock DONE!
-# set a time to go to sleep IN
-# TODO: create input fields for numbers DONE!
-# input fields should receive hours, minutes, seconds
-# these three inputs will set a timer for X milliseconds until the computer is slept
-#
-# sudo:
-# get a time (eg 2 minutes)
-# set target_time to now + 2 minutes
-# set a countdown from 2 minutes
-# if countdown == 0, go to sleep
-
-# set a time to go to sleep AT
-# TODO: create input fields for numbers
-# input fields should receive hours, minutes, seconds
-# these three inputs will then be compared to the current time, and sleep when time is reached
-#
-# sudo:
-# get a time (2PM)
-# calculate difference between now_time and target_time (2PM)
-# using the calculated time, set a countdown from that time
-# if countdown == 0, go to sleep
-
-# TODO: create a time remaining + countdown timer widget
-
-# clock only tells time
-# there's no real functionality to clock besides turning it on/off
 clock = Clock(window)
-
-header = tk.Label(text="set computer to go to sleep in:")
-header.pack(side="top")
-
-input_field_frame = tk.Frame(window)
-input_field_frame.pack(side="top")
-
-
-hms_input_field = Input_Field(input_field_frame)
-sleeper = Sleep_Manager(hms_input_field)
+input_field = Input_Field(window)
+sleeper = Sleep_Manager(input_field, clock)
 countdown = Countdown(window, sleeper)
+
+
+clock.create_clock()
+sleeper.create_sleep_controls()
+countdown.create_countdown_timer()
+
 
 set_time_input = tk.Button(text="Set Time", command=countdown.set_time_input)
 set_time_input.pack(side="left", padx="20", pady="10")
